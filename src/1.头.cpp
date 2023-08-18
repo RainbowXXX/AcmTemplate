c39b3473e3ebc0ae73b0a2a614c7abfeb0d3cb61
HM-GDTR <572579046@qq.com>
2023-08-18 21:23:44 +0800

    更新代码头

--- src/1.头.cpp
+++ src/1.头.cpp
@@ -1,7 +1,9 @@
+// luogu-judger-enable-o2
 #define _CRT_SECURE_NO_WARNINGS
 #pragma GCC optimize(3)
 #include <map>
 #include <set>
+#include <ctime>
 #include <regex>
 #include <queue>
 #include <stack>
@@ -27,19 +29,19 @@
 
 using ll = long long;
 using ull = unsigned long long;
+const double e = exp(1.0);
 const double pi = acos(-1.0);
 constexpr ll inf = 0x3f3f3f3f3f3f3f3f;
 
 constexpr int g = 3;
 constexpr char endl = '\n';
 constexpr char blank = ' ';
-constexpr double eps = 1e-9;
+constexpr long double eps = 1e-12;
 constexpr double negeps = -eps;
-constexpr ll mod = (ll)998244353;
-constexpr size_t N = (size_t)2e5 + 5;
-constexpr ull realinf = 0x7ff0000000000000;
+constexpr ll mod = (ll)1e9 + 7;
+constexpr size_t N = (size_t)1e6 + 6;
+constexpr double realinf = std::numeric_limits<double>::infinity();
 
-//代码兼容
 #define MYCALL
 #ifdef _WIN32
 #define Crash() __debugbreak()
@@ -50,4 +52,4 @@ constexpr ull realinf = 0x7ff0000000000000;
 #endif // WIN32
 
 #define ensure(expression) if(!(expression)) Crash();
-#define static_ensure(expression) static_assert(expression)
+#define static_ensure(expression) static_ensure(expression)
