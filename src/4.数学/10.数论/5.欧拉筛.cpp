d4130c1becacc0abd29fb3f6210ea7a6c9d2f1e0
HM-GDTR <572579046@qq.com>
2023-09-11 09:20:06 +0800

    更改欧拉筛的数组的命名

--- src/4.数学/10.数论/5.欧拉筛.cpp
+++ src/4.数学/10.数论/5.欧拉筛.cpp
@@ -1,14 +1,14 @@
-int prime[N], tot;
 bool not_prime[N];
+int primes[N], tot;
 
 void Sieve(int n) {
 	tot = 0;
 	not_prime[0] = not_prime[1] = 1;
 	for (int i = 2; i <= n; i++) {
-		if (not not_prime[i]) prime[tot++] = i;
-		for (int j = 0; j < tot and i * prime[j] <= n; j++) {
-			not_prime[i * prime[j]] = true;
-			if (i % prime[j] == 0) break;
+		if (not not_prime[i]) primes[tot++] = i;
+		for (int j = 0; j < tot and i * primes[j] <= n; j++) {
+			not_prime[i * primes[j]] = true;
+			if (i % primes[j] == 0) break;
 		}
 	}
 }
