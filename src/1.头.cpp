12672a370e39aeb4f933a9ac3e252cb43cff0516
HM-GDTR <572579046@qq.com>
2023-10-17 23:02:48 +0800

    更新文件头部

--- src/1.头.cpp
+++ src/1.头.cpp
@@ -19,6 +19,7 @@
 #include <numeric>
 #include <complex>
 #include <sstream>
+#include <fstream>
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
@@ -33,23 +34,23 @@ const double e = exp(1.0);
 const double pi = acos(-1.0);
 constexpr ll inf = 0x3f3f3f3f3f3f3f3f;
 
-constexpr int g = 3;
+//constexpr int g = 3;
 constexpr char endl = '\n';
 constexpr char blank = ' ';
-constexpr long double eps = 1e-12;
+constexpr long double eps = 1e-9;
 constexpr double negeps = -eps;
-constexpr ll mod = (ll)1e9 + 7;
+constexpr ll mod = (ll)998244353;
 constexpr size_t N = (size_t)1e6 + 6;
 constexpr double realinf = std::numeric_limits<double>::infinity();
 
 #define MYCALL
 #ifdef _WIN32
-#define Crash() __debugbreak()
-#define Debug() __debugbreak()
+#define Crash(...) (__debugbreak(),0)
+#define Debug(...) (__debugbreak(),0)
 #else
-#define Crash() exit(-1)
-#define Debug()
+#define Crash(...) (exit(-1),0)
+#define Debug(...) ((void)0)
 #endif // WIN32
 
-#define ensure(expression) if(!(expression)) Crash();
-#define static_ensure(expression) static_ensure(expression)
+#define ensure(expression) (void)(!!(expression)||Crash())
+#define static_ensure(expression) static_assert(expression)
